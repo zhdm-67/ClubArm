a46053c2d96598cefb115f9ebb45b46e364154f8
zhdm-67 <sheresh1251251@gmail.com>
2026-06-05 19:10:00 +0300

    Add files via upload

---
+++ Converters/BalanceToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿// Converters/BalanceToColorConverter.cs
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ClubArm.Converters
+{
+    public class BalanceToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal balance)
+            {
+                if (balance <= 0) return Brushes.Red;
+                if (balance < 5) return Brushes.Orange;
+                return Brushes.Black;
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Converters/StockToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿// Converters/StockToColorConverter.cs
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ClubArm.Converters
+{
+    public class StockToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int stock)
+            {
+                if (stock <= 0) return Brushes.Red;
+                if (stock < 5) return Brushes.Orange;
+                return Brushes.Black;
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
